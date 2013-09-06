013b22eb63346e55c0c904c05d528d0c513c253c
Tanel Suurhans <tanel@suurhans.com>
2013-09-06 16:40:28 +0300

    Remove dataType enforcing and cleanup javascript

--- vendor/assets/javascripts/jquery.remotipart.js
+++ vendor/assets/javascripts/jquery.remotipart.js
@@ -17,15 +17,15 @@
       form
         // Allow setup part of $.rails.handleRemote to setup remote settings before canceling default remote handler
         // This is required in order to change the remote settings using the form details
-        .one('ajax:beforeSend.remotipart', function(e, xhr, settings){
+        .one('ajax:beforeSend.remotipart', function(e, xhr, settings) {
           // Delete the beforeSend bindings, since we're about to re-submit via ajaxSubmit with the beforeSubmit
           // hook that was just setup and triggered via the default `$.rails.handleRemote`
           // delete settings.beforeSend;
           delete settings.beforeSend;
 
-          settings.iframe      = true;
-          settings.files       = $($.rails.fileInputSelector, form);
-          settings.data        = form.serializeArray();
+          settings.iframe   = true;
+          settings.files    = $($.rails.fileInputSelector, form);
+          settings.data     = form.serializeArray();
 
           // Insert the name/value of the clicked submit button, if any
           if (button)
@@ -33,20 +33,17 @@
 
           // jQuery 1.9 serializeArray() contains input:file entries
           // so exclude them from settings.data, otherwise files will not be sent
-          settings.files.each(function(i, file){
+          settings.files.each(function(i, file) {
             for (var j = settings.data.length - 1; j >= 0; j--)
               if (settings.data[j].name == file.name)
                 settings.data.splice(j, 1);
-          })
+          });
 
           settings.processData = false;
+          settings.data.push({ name: 'remotipart_submitted', value: true });
 
-          // Modify some settings to integrate JS request with rails helpers and middleware
-          if (settings.dataType === undefined) { settings.dataType = 'script *'; }
-          settings.data.push({name: 'remotipart_submitted', value: true});
-          if (csrfToken && csrfParam && !csrfInput) {
+          if (csrfToken && csrfParam && !csrfInput)
             settings.data.push({name: csrfParam, value: csrfToken});
-          }
 
           // Allow remotipartSubmit to be cancelled if needed
           if ($.rails.fire(form, 'ajax:remotipartSubmit', [xhr, settings])) {
@@ -74,7 +71,7 @@
     }
   };
 
-  $(document).on('ajax:aborted:file', 'form', function(){
+  $(document).on('ajax:aborted:file', 'form', function() {
     var form = $(this);
 
     remotipart.setup(form);
@@ -82,6 +79,7 @@
     // Manually call jquery-ujs remote call so that it can setup form and settings as usual,
     // and trigger the `ajax:beforeSend` callback to which remotipart binds functionality.
     $.rails.handleRemote(form);
+
     return false;
   });
 
